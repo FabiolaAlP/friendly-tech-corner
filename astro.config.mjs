a2fad31baf2396fc93ffd371ee04cab49f9c6c3d
Fabiola Alvarado <fabiolaalvarado11@gmail.com>
2024-04-17 12:39:26 -0600

    add: netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   site: "https://friendlytechcorner.netlify.app",
   integrations: [tailwind(), react()],
+  adapter: netlify()
 });
