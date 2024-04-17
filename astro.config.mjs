767f06b7369782c671e100c250d82b9b9b3ac62a
Fabiola Alvarado <fabiolaalvarado11@gmail.com>
2024-04-17 16:13:22 -0600

    test: deployment issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,24 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import netlify from "@astrojs/netlify";
+// import netlify from "@astrojs/netlify";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: netlify(),
+  // adapter: netlify(),
   site: "https://friendlytechcorner.netlify.app",
-  server: {
-    format: "directory",
-    port: 80,
-  },
+  // server: {
+  //   format: "directory",
+  //   port: 80,
+  // },
   env: {
-    WP_GRAPHQL: import.meta.env.WP_GRAPHQL,
+    WP_GRAPHQL: import.meta.env.WP_GRAPHQL
   },
+  adapter: node({
+    mode: "standalone"
+  })
 });
