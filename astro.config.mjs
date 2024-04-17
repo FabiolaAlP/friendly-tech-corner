b375bc336a10ff1c648cdb8f1136b638c403f0e5
Fabiola Alvarado <fabiolaalvarado11@gmail.com>
2024-04-17 15:37:11 -0600

    test: deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,9 @@ export default defineConfig({
   integrations: [tailwind(), react()],
   // adapter: netlify(),
   site: "https://friendlytechcorner.netlify.app",
+  server: {
+    format: "directory",
+  },
   env: {
     WP_GRAPHQL: import.meta.env.WP_GRAPHQL,
   },
