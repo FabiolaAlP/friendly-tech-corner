56b13818394506c379d7acbc075c1649cb25b192
Fabiola Alvarado <fabiolaalvarado11@gmail.com>
2024-04-17 17:02:03 -0600

    test: deployment issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,23 +3,18 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 // import netlify from "@astrojs/netlify";
-
-import node from "@astrojs/node";
+// import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "server",
-  // adapter: netlify(),
-  site: "https://friendlytechcorner.netlify.app",
+  // // output: "server",
+  // // adapter: netlify(),
+  // site: "https://friendlytechcorner.netlify.app",
   // server: {
-  //   format: "directory",
   //   port: 80,
   // },
   env: {
-    WP_GRAPHQL: import.meta.env.WP_GRAPHQL
+    WP_GRAPHQL: import.meta.env.WP_GRAPHQL,
   },
-  adapter: node({
-    mode: "standalone"
-  })
 });
