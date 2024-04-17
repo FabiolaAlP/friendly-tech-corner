79756a132c9e294971fdb0d1e06066291e735002
Fabiola Alvarado <fabiolaalvarado11@gmail.com>
2024-04-17 14:42:20 -0600

    test: deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  // output: "server",
   // site: "https://friendlytechcorner.netlify.app",
   integrations: [tailwind(), react()],
   adapter: netlify(),
