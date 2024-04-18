49e672d1e62c15e6944052ed557550e5a092a496
Fabiola Alvarado <fabiolaalvarado11@gmail.com>
2024-04-18 15:27:29 -0600

    refactor: reusable query for graphql endpoint

---
+++ src/data/wordpress.ts
@@ -0,0 +1,19 @@
+interface WPGraphqlParams {
+  query: string;
+  variables?: object;
+}
+
+export async function wpquery({ query, variables = {} }: WPGraphqlParams) {
+  const headers = { "Content-Type": "application/json" };
+  const res = await fetch(import.meta.env.WP_GRAPHQL, {
+    method: "POST",
+    headers,
+    body: JSON.stringify({ query, variables }),
+  });
+  if (!res.ok) {
+    console.error(await res.json());
+    return {};
+  }
+  const { data } = await res.json();
+  return data?.posts?.nodes ?? [];
+}
